5dba90d73b31df4f7ced0954e0975ea9347db45d
Anatole <me@anatole.dev>
2022-10-11 07:57:23 +0200

    Chat: Render Twitch Badges

---
+++ src/site/twitch.tv/modules/chat/components/ChatBadge.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="seventv-chat-badge">
+		<img :src="badge.image1x" :srcset="srcset" :alt="badge.title" />
+	</div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+	badge: Twitch.ChatBadge;
+}>();
+
+const srcset = `${props.badge.image1x} 1x, ${props.badge.image2x} 2x, ${props.badge.image4x} 4x`;
+</script>
+
+<style scoped lang="scss">
+.seventv-chat-badge {
+	display: inline-block;
+	vertical-align: middle;
+}
+</style>
