16a3d3ca5a2e35431d720613f392ed77abc80a39
Anatole <me@anatole.dev>
2023-01-10 04:18:36 +0100

    Reset legacy static assignments on entitle

--- src/composable/useCosmetics.ts
+++ src/composable/useCosmetics.ts
@@ -13,6 +13,8 @@ const data = reactive({
 
 	userBadges: {} as Record<string, SevenTV.Cosmetic<"BADGE">[]>,
 	userPaints: {} as Record<string, SevenTV.Cosmetic<"PAINT">[]>,
+
+	staticallyAssigned: {} as Record<string, Record<string, never>>,
 });
 
 let init = false;
@@ -35,13 +37,17 @@ export function useCosmetics() {
 			},
 		);
 
+		// Assign legacy V2 static cosmetics
 		target.addEventListener("static_cosmetics_fetched", (e) => {
 			const { badges, paints } = e.detail;
+
+			// Assign legacy static badges
 			for (const badge of badges) {
 				for (const u of badge.user_ids) {
 					if (data.userBadges[u]) continue;
 
 					data.userBadges[u] = [badge];
+					data.staticallyAssigned[u] = {};
 				}
 
 				badge.user_ids.length = 0;
@@ -49,11 +55,13 @@ export function useCosmetics() {
 				data.cosmetics[badge.id] = badge;
 			}
 
+			// Assign legacy static paints
 			for (const paint of paints) {
 				for (const u of paint.user_ids) {
 					if (data.userPaints[u]) continue;
 
 					data.userPaints[u] = [paint];
+					data.staticallyAssigned[u] = {};
 				}
 
 				paint.user_ids.length = 0;
@@ -70,6 +78,14 @@ export function useCosmetics() {
 	 * @param mode "+" to bind, "-" to unbind
 	 */
 	function setEntitlement(ent: SevenTV.Entitlement, mode: "+" | "-") {
+		if (data.staticallyAssigned[ent.user_id]) {
+			// If user had statically assigned cosmetics,
+			// clear them so they be properly set with live data
+			data.userBadges[ent.user_id] = [];
+			data.userPaints[ent.user_id] = [];
+			delete data.staticallyAssigned[ent.user_id];
+		}
+
 		data.entitlementBuffers[mode].push(ent);
 
 		flush();
