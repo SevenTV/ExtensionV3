bfd689faf4fdad5f6a77ca4f2077e8f11791ccc8
Jarl Skajaa Gunnarsli <jarlsgu@gmail.com>
2023-01-12 11:46:03 +0100

    Fix bits image url

--- src/common/Transform.ts
+++ src/common/Transform.ts
@@ -67,7 +67,7 @@ export function convertCheerEmote(data: Twitch.ChatMessage.Part.EmoteContent): S
 		listed: true,
 		owner: null,
 		host: {
-			url: data.images?.dark["1x"].split("/").slice(0, -1).join("/") ?? "",
+			url: data.images?.dark["1x"].split("/").slice(0, -1).join("/").replace("https:", "") ?? "",
 			files: [
 				{
 					name: "1.gif",
