85f40ae8e738f433030554a271aaca02e887964a
Excellify <Jarlsgu@gmail.com>
2023-01-12 16:42:15 +0100

    lint

--- src/common/Transform.ts
+++ src/common/Transform.ts
@@ -57,7 +57,7 @@ export function convertTwitchEmote(data: Partial<Twitch.TwitchEmote>): SevenTV.E
 	};
 }
 
-export function convertCheerEmote(data: Twitch.ChatMessage.Part.EmoteContent): SevenTV.Emote {
+export function convertCheerEmote(data: Twitch.ChatMessage.EmotePart["content"]): SevenTV.Emote {
 	return {
 		id: data.emoteID ?? "",
 		name: `${data.alt} ${data.cheerAmount}`,
